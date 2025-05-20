4d430a35845f3c6c71f0d3a0f9117de79401e456
ventriloquo <dfern3321@gmail.com>
2025-05-20 11:01:49 -0300

    Emacs

---
+++ build.el
@@ -0,0 +1,55 @@
+(require 'ox-publish)
+(load-file "./htmlize.el")
+
+(delete-directory "./public" t)
+
+(setq org-html-preamble
+      (concat
+       "<header>"
+         "<nav>"
+           "<ul>"
+             "<li><a href=\"/\">Início</a></li>"
+           "</ul>"
+	   "<ul>"
+             "<li><a href=\"/blog\">Blog</a></li>"
+           "</ul>"
+         "</nav>"
+	 "</header>"))
+
+(setq org-html-postamble nil)
+
+(setq org-html-head "<link rel=icon type=\"image/webp\" href=\"/fav.webp\"> <link rel=stylesheet href=\"/css/main.css\" />"
+      org-html-head-include-default-style nil
+      org-html-head-include-scripts nil
+      org-html-use-infojs nil
+      org-html-doctype "html5"
+      org-html-html5-fancy t)
+
+(setq org-publish-project-alist
+      '(("pages"
+    	 :base-directory "./content"
+	 :auto-sitemap t
+	 :sitemap-filename "./blog/sitemap.org"
+	 :sitemap-title "Sitemap"
+	 :sitemap-sort-files anti-chronologically
+	 :recursive t
+	 :htmlized-source t
+	 :publishing-directory "./public"
+	 :section-numbers nil
+	 :with-author nil
+	 :validate nil
+	 :language "pt-br"
+	 :publishing-function org-html-publish-to-html)
+	
+	("static"
+	 :base-directory "./assets/"
+	 :base-extension "css\\|png\\|jpg\\|webp\\|gif\\|woff2\\|otf\\|ttf"
+	 :recursive t
+	 :publishing-directory "./public"
+	 :publishing-function org-publish-attachment)
+	
+	("tukain.xyz"
+	 :components ("pages" "static"))))
+
+(org-publish-all t)
+
