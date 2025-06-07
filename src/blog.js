c84f27dbccbed52507c6461bc5c97035f01261ae
Ventriloquo <dfern3321@gmail.com>
2025-06-06 21:47:33 -0300

    Formatação (cortesia do Emacs)

--- src/blog.js
+++ src/blog.js
@@ -2,14 +2,14 @@
 
 let post, text, title, date, id;
 for (let i = 0; i < posts.length; i++) {
-  post	= posts[i];
-  title = post.title;
-  text	= post.content;
-  date	= post.date;
+    post	= posts[i];
+    title	= post.title;
+    text	= post.content;
+    date	= post.date;
 
-  if (date === undefined) {
-    date = "Sem data definida"
-  }
+    if (date === undefined) {
+	date = "Sem data definida"
+    }
 
     id = `post_${date.replaceAll(".", "-")}_${title.toLowerCase()
                                                    .replaceAll(" ", "-")
@@ -23,13 +23,13 @@ for (let i = 0; i < posts.length; i++) {
                                                    .replaceAll("ô", "o")
                                                    .replaceAll("õ", "o")}`
 
-   let article = document.createElement("article");
-   article.setAttribute("id", id);
-   document.getElementById("blog_posts").appendChild(article);
-   document.getElementById(id).innerHTML = `
-   <a href="#${id}"><h1>${title} <time>${date}</time></h1></a>
-   ${text.replaceAll("\n", "<br>")
-         .replaceAll("--", "—")}
-   <hr>`;
+    let article = document.createElement("article");
+    article.setAttribute("id", id);
+    document.getElementById("blog_posts").appendChild(article);
+    document.getElementById(id).innerHTML = `
+    <a href="#${id}"><h1>${title} <time>${date}</time></h1></a>
+    ${text.replaceAll("\n", "<br>")
+          .replaceAll("--", "—")}
+    <hr>`;
 }
 
