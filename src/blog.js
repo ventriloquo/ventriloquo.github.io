0b4cc0a9da88ac7a259ea0951e7eee33fe9f4b5c
Ventriloquo <dfern3321@gmail.com>
2025-06-13 21:45:54 -0300

    Updates

--- src/blog.js
+++ src/blog.js
@@ -1,5 +1,9 @@
 "use strict"
 
+const toc = document.createElement("ul");
+document.getElementById("blog_posts").appendChild(toc);
+toc.setAttribute("id", "table_of_contents")
+
 let post, text, title, date, id;
 for (let i = 0; i < posts.length; i++) {
     post	= posts[i];
@@ -25,7 +29,13 @@ for (let i = 0; i < posts.length; i++) {
 
     let article = document.createElement("article");
     article.setAttribute("id", id);
+
+    let toc_item = document.createElement("li");
+    toc_item.innerHTML = `<a href="#${id}">${title}</a>`
+
     document.getElementById("blog_posts").appendChild(article);
+    document.getElementById("table_of_contents").appendChild(toc_item)
+
     document.getElementById(id).innerHTML = `
     <a href="#${id}"><h1>${title} <time>${date}</time></h1></a>
     ${text.replaceAll("\n", "<br>")
@@ -33,3 +43,12 @@ for (let i = 0; i < posts.length; i++) {
     <hr>`;
 }
 
+const progressBar = document.getElementById('progress-bar');
+
+
+window.addEventListener('scroll', () => {
+  const totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrollPosition = window.scrollY;
+  const progress = (scrollPosition / totalHeight) * 100;
+  progressBar.style.width = `${progress}%`;
+});
