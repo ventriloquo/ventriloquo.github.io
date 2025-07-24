b2121de54f2abed5d36cd8249442859304b350ad
Ventriloquo <dfern3321@gmail.com>
2025-07-23 23:07:04 -0300

    Updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 "use strict";
 
-let livros_total = document.getElementById("status_biblioteca");
-livros_total.innerHTML = `<p>Total de livros: ${
-  document.getElementsByClassName("livro").length
-}</p>`;
+let livros_lidos        = 0;
+let livros_sendo_lidos  = 0;
+let livros_total        = document.getElementsByClassName("livro").length;
+let livros_progresso    = document.getElementsByTagName("progress");
+
+for (let i = 0; i < livros_progresso.length; i++) {
+    livros_progresso[i].setAttribute("id", "progress");
+    if (livros_progresso[i].value == livros_progresso[i].max) {
+	livros_lidos++;
+    } else {
+	livros_sendo_lidos++;
+    }
+}
+
+let status_biblioteca = document.getElementById("status_biblioteca");
+status_biblioteca.innerHTML = `
+  <table>
+    <thead>
+      <tr>
+        <th>Total</th>
+        <th>Lido</th>
+        <th>Lendo</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>${livros_total}</td>
+        <td>${livros_lidos}</td>
+        <td> ${livros_sendo_lidos}</td>
+      </tr>
+    </tbody>
+  </table>
+`;
 
 let post, text, title, date, id;
 for (let i = 0; i < posts.length; i++) {
