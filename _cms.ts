d2a564f1cef491d53cd2eaaae11f53a8f1b2212c
tukain <tucano@firemail.cc>
2026-05-12 13:45:50 -0300

    Updates

--- _cms.ts
+++ _cms.ts
@@ -1,5 +1,27 @@
 import lumeCMS from "lume/cms/mod.ts";
 
-const cms = lumeCMS();
+const cms = lumeCMS({
+  site: {
+    name: "Caderno do Tukain",
+    description: "Aqui é onde o conteúdo do site é gerenciado.",
+    url: "https://tukainpng.neocities.org"
+  }
+});
+
+cms.storage("src/assets", "assets");
+
+cms.collection("Posts", "src:posts/*.md", [
+  "title: text",
+  "date: date",
+  "content: markdown"
+])
+
+cms.upload("Imagens", "src:assets/img");
+
+cms.document("Projetos", "src:_data/projects.yml")
+cms.document("Livros", "src:_data/books.yml")
+cms.document("Jogos", "src:_data/games.yml")
+cms.document("Ideias", "src:_data/ideas.yml")
+cms.document("Links", "src:_data/buttons.yml")
 
 export default cms;
