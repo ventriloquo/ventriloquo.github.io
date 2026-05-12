731d351cf263e8b279255a6d68ffe75b04dc51c2
tukain <tucano@firemail.cc>
2026-05-12 13:08:54 -0300

    Updates

--- _config.ts
+++ _config.ts
@@ -1,4 +1,15 @@
 import lume from "lume/mod.ts";
+import feed from "lume/plugins/feed.ts";
+import date from "lume/plugins/date.ts"
+import prism from "lume/plugins/prism.ts";
+import "npm:prismjs@1.29.0/components/prism-rust.js";
+import "npm:prismjs@1.29.0/components/prism-go.js";
+import "npm:prismjs@1.29.0/components/prism-c.js";
+import "npm:prismjs@1.29.0/components/prism-haskell.js";
+import "npm:prismjs@1.29.0/components/prism-bash.js";
+import "npm:prismjs@1.29.0/components/prism-lisp.js";
+import "npm:prismjs@1.29.0/components/prism-lua.js";
+import "npm:prismjs@1.29.0/components/prism-makefile.js";
 
 const site = lume({
   src: "./src",
@@ -11,6 +22,23 @@ const site = lume({
     page404: "not_found.html",
   },
 });
+
+site.use(date())
+
+site.use(prism())
+
+site.use(feed({
+  output: ["/feed.rss", "/feed.json"],
+  query: "type=post",
+  info: {
+    title: "=site.title",
+    description: "=site.description",
+  },
+  items: {
+    title: "=title",
+    description: "=excerpt",
+  },
+}));
 
 site.add("assets")
 
