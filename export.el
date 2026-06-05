6f30c57597319e33aea20b203faa5e38d2bdeb1d
tukain <tucano@firemail.cc>
2026-06-05 15:54:25 -0300

    Updates

--- export.el
+++ export.el
@@ -8,12 +8,12 @@
 	    (org-publish-find-title entry project))))
 
 (setq  current_theme	(format " %s " (nth 0 custom-enabled-themes))
-       accent_color	(face-attribute 'dired-directory	:foreground nil 'default)
+       accent_color	(face-attribute 'font-lock-string-face	:foreground nil 'default)
        background_01	(face-attribute 'dired-directory	:background nil 'default)
        background_00	(face-attribute 'default		:background)
        foreground	(face-attribute 'default		:foreground)
 
-       cursor-color-as-accent-color (concat "\n:root {\n\t--ac:\t"   (face-attribute 'cursor  :background nil 'default) ";\n}\n")
+       cursor-color-as-accent-color (concat "\n:root {\n\t--ac:\t"   (face-attribute 'cursor  :background nil 'default) "!important  ;\n}\n")
        hl-line-as-background-0	    (concat "\n:root {\n\t--bg-0:\t" (face-attribute 'hl-line :background nil 'default) ";\n}\n")
        hl-line-as-background-1	    (concat "\n:root {\n\t--bg-1:\t" (face-attribute 'hl-line :background nil 'default) ";\n}\n")
        
@@ -28,16 +28,29 @@
 			    "\t--ac:\t"   accent_color	";\n"
 			    "}\n"
 			    (cond
-			     ((equal current_theme " base16-gruvbox-dark ")
-			      (concat hl-line-as-background-0))
-			     ((equal current_theme " base16-gruvbox-light ")
-			      (concat hl-line-as-background-0))
-			     ((equal current_theme " base16-nord ")
-			      (concat hl-line-as-background-0))
+			     ((or
+			       (equal current_theme " leuven-dark ")
+			       (equal current_theme " base16-gruvbox-material-dark-soft ")
+			       (equal current_theme " base16-everforest ")
+			       (equal current_theme " base16-everforest-dark-hard ")
+			       (equal current_theme " base16-everforest-dark-soft "))
+			      hl-line-as-background-1)
+
+			     ((or
+			       (equal current_theme " base16-gruvbox-dark ")
+			       (equal current_theme " base16-gruvbox-material-dark-hard ")
+			       (equal current_theme " base16-gruvbox-material-dark-medium ")
+			       (equal current_theme " base16-gruvbox-light ")
+			       (equal current_theme " base16-gotham ")
+			       (equal current_theme " base16-nord "))
+			      hl-line-as-background-0)
+
 			     ((equal current_theme " base16-sakura ")
 			      (concat cursor-color-as-accent-color hl-line-as-background-0))
+
 			     ((equal current_theme " srcery ")
 			      (concat cursor-color-as-accent-color hl-line-as-background-1))
+			     
 			     ((equal current_theme " modus-operandi ")
 			      (concat ":root {--bg-0:#e0e0e0;--bg-1:#f2f2f2;--ac:#003497;}"))
 			     ((equal current_theme " modus-vivendi ")
@@ -56,36 +69,36 @@
 
        org-html-preamble (concat
 			  "<div class='container menu'>"
-			    "<img loading='lazy' src='/assets/fav.png' height='100' style='border-radius: 5px'>"
-			    "<div class='links'>"
-			        "<a href='/'>Início</a>"			      
-			      "<div>"
-			        "<a target='_blank' href='https://neocities.org/site/tukainpng'>Neocities</a>"
-			        "<a target='_blank' href='https://codeberg.org/tukain'>Codeberg</a>"
-			      "</div>"
-			    "</div>"
+			  "<img loading='lazy' src='/assets/fav.png' height='100' style='border-radius: 5px'>"
+			  "<div class='links'>"
+			  "<a href='/'>Início</a>"			      
+			  "<div>"
+			  "<a target='_blank' href='https://neocities.org/site/tukainpng'>Neocities</a>"
+			  "<a target='_blank' href='https://codeberg.org/tukain'>Codeberg</a>"
+			  "</div>"
+			  "</div>"
 			  "</div>"
 			  "<div class='container'>"
-			    "<div class='buttons'>"
-			      "<a href='https://dorival.neocities.org/'><img loading='lazy' src='/assets/dorival-button.gif'></a>"
-			      "<a href='https://kuroi.neocities.org/'><img loading='lazy' src='/assets/kuroi.png'></a>"
-			      "<a href='https://dudalog.neocities.org/'><img loading='lazy' src='/assets/dudalog.png'></a>"
-			      "<a href='https://fi-do-manin.neocities.org/'><img loading='lazy' src='/assets/fidomanin.png'></a>"
-			      "<a href='https://sqrsux.neocities.org/'><img loading='lazy' src='/assets/sqr.gif'></a>"
-			      "<a href='https://blanketfort.neocities.org/'><img loading='lazy' src='/assets/blanket_fort.gif'></a>"
-			      "<a href='https://dabric.xyz/'><img loading='lazy' src='/assets/dabric.png'></a>"
-			    "</div>"
+			  "<div class='buttons'>"
+			  "<a href='https://dorival.neocities.org/'><img loading='lazy' src='/assets/dorival-button.gif'></a>"
+			  "<a href='https://kuroi.neocities.org/'><img loading='lazy' src='/assets/kuroi.png'></a>"
+			  "<a href='https://dudalog.neocities.org/'><img loading='lazy' src='/assets/dudalog.png'></a>"
+			  "<a href='https://fi-do-manin.neocities.org/'><img loading='lazy' src='/assets/fidomanin.png'></a>"
+			  "<a href='https://sqrsux.neocities.org/'><img loading='lazy' src='/assets/sqr.gif'></a>"
+			  "<a href='https://blanketfort.neocities.org/'><img loading='lazy' src='/assets/blanket_fort.gif'></a>"
+			  "<a href='https://dabric.xyz/'><img loading='lazy' src='/assets/dabric.png'></a>"
+			  "</div>"
 			  "</div>")
 
        org-html-postamble (concat
-			  "<div class='container'>"
-			    "<img loading='lazy' style='margin: 0' src='/assets/under_construction.png'>"
-			  "</div>"
-			  "<div class='container'>"
-			    "<p>Gerado com: %c</p>"
-			    "<p>Tema: " current_theme "</p>"
-			    "<p>Autor do site: <a href='https://codeberg.org/tukain/'>Tukain</a></p>"
-			  "</div>")
+			   "<div class='container'>"
+			   "<img loading='lazy' style='margin: 0' src='/assets/under_construction.png'>"
+			   "</div>"
+			   "<div class='container'>"
+			   "<p>Gerado com: %c</p>"
+			   "<p>Tema: " current_theme "</p>"
+			   "<p>Autor do site: <a href='https://codeberg.org/tukain/'>Tukain</a></p>"
+			   "</div>")
        
        org-export-with-section-numbers	nil
        org-export-with-toc		3
